b77c7ec823ed07a534ad6591e1b14304af62a5a4
hueyjj <jasperjeng@gmail.com>
2018-03-30 00:45:10 -0700

    Test for downloading video with default options

--- cherry_dl/core/downloadertest.py
+++ cherry_dl/core/downloadertest.py
@@ -1,11 +1,25 @@
-from downloader import MetaInformation
+from downloader import MetaInformation, Downloader
 
 url = r"https://www.youtube.com/watch?v=eHir_vB1RUI"
-m = MetaInformation()
-m.load(url)
+#m = MetaInformation()
 
-print(type(m.title))
-#print(m.title)
-#print(m.description)
-print(m.thumbnail)
+#print(type(m.title))
+##print(m.title)
+##print(m.description)
+#print(m.thumbnail)
+
+defaultYoutubeOpts = {
+    "format": "m4a",
+    "ignoreerrors": True,
+    "writethumbnail": True,
+    "outtmpl": "%(title)s.%(ext)s",
+    "postprocessors": [{
+        "key": "EmbedThumbnail",
+    }],
+}
+dl = Downloader(url, r"C:/users/jj/downloads", defaultYoutubeOpts)
+dl.run()
+
+
+
 
