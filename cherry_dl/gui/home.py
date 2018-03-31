6b4830cc821b858667e36e8025a766577e488386
hueyjj <jasperjeng@gmail.com>
2018-03-31 04:54:29 -0700

    Moved downloading logic to home widgett

--- cherry_dl/gui/home.py
+++ cherry_dl/gui/home.py
@@ -7,7 +7,9 @@ from PyQt5.QtCore import (
     Qt, 
     QCoreApplication, 
     QThread, 
-    pyqtSignal
+    QThreadPool,
+    pyqtSignal,
+    pyqtSlot,
 )
 from PyQt5.QtWidgets import (
     QApplication, 
@@ -31,21 +33,99 @@ from PyQt5.QtWidgets import (
     QListWidget,
     QRadioButton,
     QCheckBox,
+    QToolButton,
+    QMainWindow,
+    QAction,
+    QSizePolicy,
 )
 
 from PyQt5.QtGui import (
     QIcon, 
     QCursor, 
     QWindow, 
-    QGuiApplication
+    QGuiApplication,
+    QPixmap,
 )
 
-class Home(QWidget):
+from ..ui.home_ui import Ui_Home
+from ..core.downloader import (
+    MetaInformation,
+    Downloader,
+)
 
-    def __init__(self, parent=None):
-        super().__init__()
 
-        layout = QFormLayout()
-        layout.addRow("Name", QLineEdit())
-        layout.addRow("Address", QLineEdit())
-        self.setLayout(layout)
+class Home(QWidget, Ui_Home):
+
+    metaThreadPool = QThreadPool()
+
+    # FIXME Embedding thumbnail requires ffmpeg and atomicparsely. Include as dependencies in project later?
+    defaultYoutubeOpts = {
+        "format": "m4a",
+        "ignoreerrors": True,
+        "writethumbnail": True,
+        "outtmpl": "%(title)s.%(ext)s",
+        "postprocessors": [{
+            "key": "EmbedThumbnail",
+        }],
+    }
+    userYoutubeOpts = {
+    }
+
+    def __init__(self, parent):
+        super().__init__(parent)
+
+        self.setupUi(self)
+
+        self.userInput.textChanged.connect(self.urlChanged)
+        self.download.clicked.connect(self.startDownload)
+
+        self.description.setReadOnly(True)
+
+    def startMetaDownloadThread(self):
+        url = self.userInput.text()
+
+        meta = MetaInformation(url)
+        meta.signal.metaDownloaded.connect(self.loadMetaInfo)
+        meta.signal.unableToRetrieveMeta.connect(self.noMetaInformationFound)
+
+        self.metaThreadPool.start(meta)
+
+    def startDownload(self):
+        # validate(self.userInput.text())
+        url = self.userInput.text()
+
+        dl = Downloader(url, r"C:/users/jj/downloads", self.defaultYoutubeOpts)
+
+        self.metaThreadPool.start(dl)
+
+    @pyqtSlot(str)
+    def urlChanged(self, text):
+        # FIXME validate url later
+        self.startMetaDownloadThread()
+
+    @pyqtSlot(dict)
+    def loadMetaInfo(self, meta):
+        print("Loaded meta information")
+
+        # Title
+        self.title.setText(meta["title"])
+
+        # Description
+        self.description.setText("""
+        <p>
+        %s
+        </p>
+        """ % meta["description"])
+
+        # Image (load from a byte array)
+        pic = QPixmap()
+        pic.loadFromData(meta["thumbnail"])
+        pic = pic.scaledToWidth(self.width())
+        self.image.setPixmap(pic)
+
+    @pyqtSlot(str)
+    def noMetaInformationFound(self, url):
+        if url:
+            print("No meta information found for " + url)
+        else:
+            print("No meta information found and no url provided.")
