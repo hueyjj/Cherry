c4700900dedc4f968261549f9b9536be02674cc9
hueyjj <jasperjeng@gmail.com>
2018-03-31 01:30:18 -0700

    Build separate ui for widget

--- buildui.sh
+++ buildui.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env sh
-pyuic5.exe -o cherry_dl/gui/cherryui.py designer/CherryDesigner/mainwindow.ui
+
+# Generate main window
+pyuic5.exe -o cherry_dl/ui/main_ui.py designer/CherryDesigner/mainwindow.ui
+# Generate home widget
+pyuic5.exe -o cherry_dl/ui/home_ui.py designer/CherryDesigner/home.ui
+# Generate progress widget
+pyuic5.exe -o cherry_dl/ui/progress_ui.py designer/CherryDesigner/progress.ui
+# Generate home widget
+pyuic5.exe -o cherry_dl/ui/history_ui.py designer/CherryDesigner/history.ui
+# Generate configuration widget
+pyuic5.exe -o cherry_dl/ui/config_ui.py designer/CherryDesigner/configuration.ui
