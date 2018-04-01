81c1f6dc951f549a0978b8c9f1d9c56bbd531f00
hueyjj <jasperjeng@gmail.com>
2018-04-01 00:24:31 -0700

    Rearrange methods order and add setSaveDir method

--- cherry_dl/core/utils.py
+++ cherry_dl/core/utils.py
@@ -1,16 +1,17 @@
 import os
 import json
 
-class Config(object):
-    homedir = os.path.expanduser('~')
-    appdir = ".cherry-dl"
-    savedir = os.path.join(homedir, appdir)
 
-    userPath = os.path.join(savedir, "config.json")
-    defaultPath = os.path.join(homedir, "Downloads")
+class Config(object):
+    homeDir = os.path.expanduser('~')
+    appDirName = ".cherry-dl"
+    userSaveDir = os.path.join(homeDir, appDirName)
+
+    userConfigPath = os.path.join(userSaveDir, "config.json")
+    defaultSavePath = os.path.join(homeDir, "Downloads")
 
     defaultConfig = {
-        "saveDirectory": defaultPath,
+        "saveDirectory": defaultSavePath,
         "youtubedl": {
             "format": "m4a",
             "ignoreerrors": "True",
@@ -24,28 +25,39 @@ class Config(object):
 
     def __init__(self):
         # Check if user configuration exists
-        if os.path.exists(self.userPath) and os.path.isfile(self.userPath):
+        if os.path.exists(self.userConfigPath) and os.path.isfile(self.userConfigPath):
             self.loadUserConfig()
         else:
             self.loadDefaultConfig()
-        
-    def getConfig(self):
-        return self.config
-    
-    def getYoutubedlConfig(self):
-        return self.config["youtubedl"]
 
     def loadUserConfig(self):
-        with open(self.userPath, 'r') as infile:
+        with open(self.userConfigPath, 'r') as infile:
             self.config = json.load(infile)
-    
+
     def loadDefaultConfig(self):
+        if not os.path.exists(self.userSaveDir):
+            os.makedirs(self.userSaveDir)
+
         self.config = self.defaultConfig
         self.saveConfig()
 
+    def resetConfig(self):
+        self.loadDefaultConfig()
+
     def saveConfig(self):
-        with open(self.userPath, 'w') as outfile:
+        with open(self.userConfigPath, 'w') as outfile:
             json.dump(self.config, outfile, indent=True)
-    
+
+    def getConfig(self):
+        return self.config
+
+    def getYoutubedlConfig(self):
+        return self.config["youtubedl"]
+
+    def setSaveDir(self, newDir):
+        self.config["saveDirectory"] = newDir
+
     def setFormat(self, newFormat):
         self.getYoutubedlConfig()["format"] = newFormat
+
+    # TODO Set video, audio, or both
