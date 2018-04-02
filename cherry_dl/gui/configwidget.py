71d40cd97eddd9a477535c2ec3068ccbee16fac2
hueyjj <jasperjeng@gmail.com>
2018-04-01 23:21:43 -0700

    User can set format options and will emit to whoever connected to the singals.

--- cherry_dl/gui/configwidget.py
+++ cherry_dl/gui/configwidget.py
@@ -1,16 +1,24 @@
 from PyQt5 import (
+    QtCore,
     QtWidgets,
+)
+
+from PyQt5.QtCore import (
+    pyqtSignal,
+    pyqtSlot,
 )
 
 from PyQt5.QtWidgets import (
     QApplication,
     QWidget,
+    QRadioButton,
 )
 
 from ..ui.config_ui import Ui_Configuration
 from ..core.utils import Config
 
 class ConfigWidget(QWidget, Ui_Configuration):
+    formatChanged = pyqtSignal()
 
     def __init__(self, parent):
         super().__init__(parent)
@@ -24,3 +32,32 @@ class ConfigWidget(QWidget, Ui_Configuration):
         c = self.config.getConfig()
         self.dirInput.setText(c["saveDirectory"])
 
+        # Enable format radio button
+        f = c["youtubedl"]["format"]
+        if f == "mp3":
+            self.mp3.setChecked(True)
+        elif f == "m4a":
+            self.m4a.setChecked(True)
+        elif f == "mp4":
+            self.mp4.setChecked(True)
+
+        # Enable video radio button
+
+        # Connect signals to options
+        for formatOption in self.formatGroup.children():
+            if isinstance(formatOption, QRadioButton):
+                formatOption.clicked.connect(self.setFormat)
+        
+        self.apply.clicked.connect(self.saveConfig)
+    
+    def setFormat(self):
+        sender = self.sender()
+        if sender is self.mp3:
+            self.config.setFormat("mp3")
+        elif sender is self.m4a:
+            self.config.setFormat("m4a")
+        elif sender is self.mp4:
+            self.config.setFormat("mp4")
+
+    def saveConfig(self):
+        self.config.saveConfig()
