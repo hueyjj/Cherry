89a33be7fce82c1a946d4801179545ea1c5c6d5b
hueyjj <jasperjeng@gmail.com>
2018-04-03 22:10:44 -0700

    Rebuild ui and resources if necessary before building

--- build_cherry-dl.sh
+++ build_cherry-dl.sh
@@ -1,6 +1,11 @@
 #!/usr/bin/env sh
 #TODO Include python optimization https://pyinstaller.readthedocs.io/en/stable/usage.html#running-pyinstaller-with-python-optimizations
 
+echo "Converting .ui files to .py files"
+./build_ui.sh
+echo "Converting .qrc files to .py files"
+./build_resources.sh
+
 pyinstaller cherry_dl/__main__.py \
     -n cherry-dl \
     --noconfirm \
